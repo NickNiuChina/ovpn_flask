1a2e14d8b70c9ddcc39afe72a23ce08746064022
NickNiuChina <nick.niu.china@gmail.com>
2023-08-25 16:58:58 +0800

    update

--- myproject/ovpn.py
+++ myproject/ovpn.py
@@ -23,29 +23,16 @@ bp = Blueprint("ovpn", __name__, url_prefix='/')
 #################################################
 # test pages
 #################################################
-@bp.route("/test/test", methods=('GET', 'POST'))
+@bp.route("/test", methods=('GET', 'POST'))
 def test():
-    """test page
+    """
+    test page
         This is for test purpose only.
 
     Returns:
         list: flask will take the list or dict and turns to jason automatically.
     """
-    cur = get_cur
-    cur.execute(
-        "SELECT * FROM tb_test"
-    )
-    test_list = []
-    
-    datasets = cur.fetchall()
-    for row in datasets:
-        temp_list = []
-        for r in row:
-            temp_list.append(r)
-        test_list.append(temp_list)
-    
-    return {"tb_test": datasets}, 200
-    # return "Hello 你好！", 200 # return string
+    return "Hello 世界！", 200
 
 #################################################
 # Login page
@@ -108,6 +95,21 @@ def introduction():
         template: introduction template
     """
     return render_template("ovpn/introduction.html")
+
+#################################################
+# tips view
+#################################################
+
+@bp.route("/tips")
+@login_required
+def tips():
+    """tips page
+
+    Returns:
+        template: tips template
+    """
+    return render_template("ovpn/tips.html")
+
 
 #################################################
 # admin views
@@ -1034,130 +1036,4 @@ def toggleUser():
     
     success = "Toggle user successfully. User: " + target_user
     flash(success, 'success')    
-    return redirect(url_for("ovpn.adminUser"))    
-         
-#################################################
-# User views
-#################################################
-
-@bp.route("/userScore")
-@login_required
-def userScore():
-    """introduction page
-
-    Returns:
-        template: introduction template
-    """
-    return render_template("ovpn/introduction.html")                    
-
-@bp.route("/userInfo")
-@login_required
-def userInfo():
-    """introduction page
-
-    Returns:
-        template: introduction template
-    """
-    return render_template("ovpn/introduction.html")   
-
-#################################################
-# Others, examples
-#################################################
-
-def get_post(id, check_author=True):
-    """Get a post and its author by id.
-
-    Checks that the id exists and optionally that the current user is
-    the author.
-
-    :param id: id of post to get
-    :param check_author: require the current user to be the author
-    :return: the post with author information
-    :raise 404: if a post with the given id doesn't exist
-    :raise 403: if the current user isn't the author
-    """
-    post = (
-        get_db()
-        .execute(
-            "SELECT p.id, title, body, created, author_id, username"
-            " FROM post p JOIN user u ON p.author_id = u.id"
-            " WHERE p.id = ?",
-            (id,),
-        )
-        .fetchone()
-    )
-
-    if post is None:
-        abort(404, "Post id {0} doesn't exist.".format(id))
-
-    if check_author and post["author_id"] != g.user["id"]:
-        abort(403)
-
-    return post
-
-@bp.route("/create", methods=("GET", "POST"))
-@login_required
-def create():
-    """Create a new post for the current user."""
-    if request.method == "POST":
-        title = request.form["title"]
-        body = request.form["body"]
-        error = None
-
-        if not title:
-            error = "Title is required."
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
-            )
-            db.commit()
-            return redirect(url_for("blog.index"))
-
-    return render_template("blog/create.html")
-
-
-@bp.route("/<int:id>/update", methods=("GET", "POST"))
-@login_required
-def update(id):
-    """Update a post if the current user is the author."""
-    post = get_post(id)
-
-    if request.method == "POST":
-        title = request.form["title"]
-        body = request.form["body"]
-        error = None
-
-        if not title:
-            error = "Title is required."
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for("blog.index"))
-
-    return render_template("blog/update.html", post=post)
-
-
-@bp.route("/<int:id>/delete", methods=("POST",))
-@login_required
-def delete(id):
-    """Delete a post.
-
-    Ensures that the post exists and that the logged in user is the
-    author of the post.
-    """
-    get_post(id)
-    cur = get_cur()
-    cur.execute("DELETE FROM post WHERE id = ?", (id,))
-    cur.commit()
-    return redirect(url_for("blog.index"))
+    return redirect(url_for("ovpn.adminUser"))          
