50c3a2606c799d1d3fd557800af92448cabf4595
NickNiuChina <nick.niu.china@gmail.com>
2024-07-18 22:59:05 +0800

    Update ovpn.py

--- orm/ovpn.py
+++ orm/ovpn.py
@@ -34,11 +34,10 @@ class User(Base):
         return f"User(id={self.id!r}, name={self.name!r})"
     
 
-# class UserGroup(Base):
-#     __tablename__ = "address"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     email_address: Mapped[str]
-#     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-#     user: Mapped["User"] = relationship(back_populates="addresses")
-#     def __repr__(self) -> str:
-#         return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+class UserGroup(Base):
+    __tablename__ = "user_group"
+    id: Mapped[int] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    group: Mapped[str] = mapped_column(String(50))
+    
+    def __repr__(self) -> str:
+        return f"Group(id={self.id!r}, group={self.group!r})"
