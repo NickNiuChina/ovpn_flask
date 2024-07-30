0f49a3f45f153f79d0b04dda3e23eade274fc2dc
NickNiuChina <nick.niu.china@gmail.com>
2024-07-30 16:58:28 +0800

    Update ovpn.py

--- orm/ovpn.py
+++ orm/ovpn.py
@@ -13,8 +13,11 @@ from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, ForeignKeyC
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.types import types
+from sqlalchemy.sql import func
+
+import datetime
+
 
 Base = declarative_base()
 
@@ -73,7 +76,7 @@ class UserGroup(Base):
         UniqueConstraint('group'),
     )
     
-    id: Mapped[int] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     group: Mapped[str] = mapped_column(String(50))
     
     def __repr__(self) -> str:
@@ -91,7 +94,7 @@ class OvpnServers(Base):
         UniqueConstraint('server_name'),
     )
     
-    id: Mapped[int] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     server_name: Mapped[str] = mapped_column(String(50))
     configuration_dir: Mapped[str] = mapped_column(String(200))
     configuration_file: Mapped[str] = mapped_column(String(200))
@@ -106,68 +109,81 @@ class OvpnServers(Base):
     management_port: Mapped[int] = mapped_column(Integer)
     management_password: Mapped[str] = mapped_column(String(100))
     comment: Mapped[str] = mapped_column(Text(1024))
-    creation_time = models.DateTimeField(default=datetime.datetime.now, null=False, blank=False)
-    update_time = models.DateTimeField(auto_now=True, null=False, blank=False)
+    creation_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    update_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
 
-
-class ClientList(models.Model):
-    """OpenVPN client model"""
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    server = models.ForeignKey(
-        Servers,
-        verbose_name=_('server'),
-        blank=False,
-        null=False,
-        on_delete=models.CASCADE)
-    site_name = models.CharField(max_length=100, null=True, blank=True)
-    cn = models.CharField(max_length=100, null=False, blank=False, unique=True)
-    ip = models.CharField(max_length=20, null=False, blank=False, unique=True)
-    toggle_time = models.DateTimeField(default=now)
+class OvpnClientList(Base):
+    """
+    OpenVPN client model
+    """
+    __tablename__ = "ovpn_clients_list"
+    __table_args__ = (
+        UniqueConstraint('cn'),
+    )
+    
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    server: Mapped[UUID] = mapped_column(
+        ForeignKey("ovpn_servers.id")
+    )
+    site_name: Mapped[str] = mapped_column(String(100))
+    cn: Mapped[str] = mapped_column(String(100))
+    ip: Mapped[str] = mapped_column(String(100))
+    toggle_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now()
+        )
     STATUS_CHOICE = [(0, "offline"), (1, "online")]
     ENABLED_CHOICE = [(0, "disabled"), (1, "enabled")]
-    enabled = models.IntegerField(choices=ENABLED_CHOICE, default=1)
-    status = models.IntegerField(choices=STATUS_CHOICE, default=1)
-    expire_date = models.DateField(default=datetime.datetime(1970, 1, 1))
-    create_time = models.DateTimeField(_('creation time'), default=now)
-    update_time = models.DateTimeField(_('modify time'), default=now)
-
-
-class ClientListConfig(models.Model):
-    """OpenVPN client proxy config model"""
-    validate_comma_separated_integer_list = int_list_validator(
-        message=_("Enter only digits separated by commas."),
+    enabled: Mapped[int] = ChoiceType({0: "disabled", 1: "enabled"})
+    status: Mapped[int] = ChoiceType({0: "disabled", 1: "enabled"})
+    expire_date: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )  # (1970, 1, 1))
+    create_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
     )
+    update_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+
+class ClientListConfig(Base):
+    """
+    OpenVPN client proxy config model
+    """
+
     OS_TYPE_CHOICE = [(0, "Linux"), (1, "Windows"), (2, "MacOS"), (3, "Others")]
     PROXY_CHOICE = [(0, "disabled"), (1, "enabled")]
     
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    ovpn_client = models.ForeignKey(
-        ClientList,
-        verbose_name=_('ovpn_client'),
-        blank=False,
-        null=False,
-        on_delete=models.CASCADE)
-    os_type = models.IntegerField(choices=OS_TYPE_CHOICE, default=0, null=True, blank=True)
-    http_proxy = models.IntegerField(choices=PROXY_CHOICE, default=0, null=True, blank=True)
-    https_proxy = models.IntegerField(choices=PROXY_CHOICE, default=0, null=True, blank=True)
-    http_port = models.CharField(validators=[validate_comma_separated_integer_list], max_length=200, null=True, blank=True)
-    https_port = models.CharField(validators=[validate_comma_separated_integer_list], max_length=200, null=True, blank=True)
-    http_proxy_template = models.TextField(max_length=2000, null=True, blank=True)
-    ssh_proxy = models.IntegerField(choices=PROXY_CHOICE, default=0)
-    ssh_proxy_port = models.IntegerField(
-        null=True,
-        blank=True,
-        validators=[MinValueValidator(22), MaxValueValidator(65536)]
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    ovpn_client = Mapped[UUID] = mapped_column(
+        ForeignKey("ovpn_servers.id")
     )
-    create_time = models.DateTimeField(_('creation time'), default=now)
-    update_time = models.DateTimeField(_('modify time'), default=now)
+    os_type: Mapped[int] = ChoiceType({0: "Linux", 1: "Windows", 2: "MacOS", 3: "Others"})
+    http_proxy: Mapped[int] = ChoiceType({0: "Disabled", 1: "Enabled"})
+    https_proxy: Mapped[int] = ChoiceType({0: "Disabled", 1: "Enabled"})
+    http_port: Mapped[str] = mapped_column(String(100))
+    https_port: Mapped[str] = mapped_column(String(100))
+    http_proxy_template: Mapped[text] = mapped_column(Text(1024))
+    ssh_proxy: Mapped[int] = ChoiceType({0: "Disabled", 1: "Enabled"})
+    ssh_proxy_port: Mapped[int] = mapped_column(Integer)
+    create_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    update_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
     
     
 class SystemCommonConfig(Base):
     """System wide common config model"""
     PROXY_SERVER_CHOICE = [(0, "nginx"), (1, "apache")]
     
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     proxy_server = models.IntegerField(choices=PROXY_SERVER_CHOICE, default=1, null=True, blank=True)
     plain_req_file_dir = models.CharField(max_length=200, null=False, blank=False, default="plain_reqs")
     encrypt_req_file_dir = models.CharField(max_length=200, null=False, blank=False, default="encrypt_reqs")
